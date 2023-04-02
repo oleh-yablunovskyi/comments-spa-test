216210740183be2d016267e3c88b5816d0a86339
Oleh Yablunovskyi <cowperwood777@gmail.com>
2023-04-02 19:11:40 +0300

    External database was connected

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -1,11 +1,19 @@
 /* eslint-disable no-console */
 'use strict';
 
+require('dotenv').config();
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'postgres', '12345', {
-  host: 'localhost',
-  dialect: 'postgres',
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_DIALECT } = process.env;
+
+if (!DB_NAME || !DB_USER || !DB_PASSWORD || !DB_HOST || !DB_DIALECT) {
+  throw new Error('Missing required env variables for the db connection.');
+}
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: DB_DIALECT,
 });
 
 module.exports = sequelize;
